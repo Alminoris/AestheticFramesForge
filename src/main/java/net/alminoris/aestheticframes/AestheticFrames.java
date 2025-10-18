3b56dec189153d4fe5b160e23cac5f5d42f5072e
AOA <aoa@d.co>
2025-10-18 10:33:30 +0300

    1.19.4 commit

--- src/main/java/net/alminoris/aestheticframes/AestheticFrames.java
+++ src/main/java/net/alminoris/aestheticframes/AestheticFrames.java
@@ -4,12 +4,14 @@ import com.mojang.logging.LogUtils;
 import net.alminoris.aestheticframes.block.ModBlocks;
 import net.alminoris.aestheticframes.item.ModItemGroups;
 import net.alminoris.aestheticframes.item.ModItems;
+import net.alminoris.aestheticframes.util.helper.BlockSetsHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,8 +31,6 @@ public class AestheticFrames
 
         modEventBus.addListener(this::commonSetup);
 
-        ModItemGroups.register(modEventBus);
-
         ModItems.register(modEventBus);
 
         ModBlocks.register(modEventBus);
@@ -47,9 +47,162 @@ public class AestheticFrames
 
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    private void addCreative(CreativeModeTabEvent.BuildContents entries)
     {
+        if (entries.getTab() == ModItemGroups.AFS_TAB)
+        {
+            entries.accept(ModBlocks.WHITENED_WHITE_TERRACOTTA.get());
 
+            for(String name : BlockSetsHelper.WOODS)
+            {
+                for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                    entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+            }
+
+            if (ModList.get().isLoaded("arborealnature"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("whisperleaftrees"))
+            {
+                for(String name : BlockSetsHelper.WT_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("silverwoodtrees"))
+            {
+                for(String name : BlockSetsHelper.ST_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("missingtrees"))
+            {
+                for(String name : BlockSetsHelper.MT_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("natures_spirit"))
+            {
+                for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            for(String name : BlockSetsHelper.STONES)
+            {
+                for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                    entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_STONES_WF)
+                {
+                    for (String frameName : BlockSetsHelper.FRAME_TYPES)
+                        entries.accept(ModBlocks.FRAMES.get(frameName+name).get());
+                }
+            }
+
+            for(String name : BlockSetsHelper.WOODS)
+            {
+                for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                    entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+            }
+
+            if (ModList.get().isLoaded("arborealnature"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_AN)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_WOODS_WF)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("whisperleaftrees"))
+            {
+                for(String name : BlockSetsHelper.WT_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("silverwoodtrees"))
+            {
+                for(String name : BlockSetsHelper.ST_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("missingtrees"))
+            {
+                for(String name : BlockSetsHelper.MT_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            if (ModList.get().isLoaded("natures_spirit"))
+            {
+                for(String name : BlockSetsHelper.NSS_WOOD_NAMES)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+
+            for(String name : BlockSetsHelper.STONES)
+            {
+                for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                    entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+            }
+
+            if (ModList.get().isLoaded("wildfields"))
+            {
+                for(String name : BlockSetsHelper.EXTRA_STONES_WF)
+                {
+                    for (String frameName : BlockSetsHelper.SCALED_FRAME_TYPES)
+                        entries.accept(ModBlocks.SCALED_FRAMES.get(frameName+name).get());
+                }
+            }
+        }
     }
 
     @SubscribeEvent
